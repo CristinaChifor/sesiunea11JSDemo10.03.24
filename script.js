ee86541fa4aa001c651bd42ca9d48f0f6b16c10d
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-03-12 14:24:48 +0200

    Add Object Data Examples

--- script.js
+++ script.js
@@ -69,7 +69,60 @@ console.log(`After concatenation of subset`, concatenatedArrays);
 let person = {
     name: "John",
     surname: "Doe",
-    
+    address: {
+        street: "Cetatii",
+        number: 21,
+        appartment: "B1",
+        block: "7B",
+        tronson: 1,
+        zipCode: "1002023",
+        city: "Cluj",
+        country: "Romania",
+        county: "Cluj-Napoca"
+    },
+    age: 30
+
 };
+
+console.log(`person:`, person);
+console.log(`nume persoana: ${person.name}`);
+console.log(`cod postal ${person.address.zipCode}`);
+console.log(`surname ${person.surname}`);
+//console.log(person.appartment); returneaza undefined
+console.log(person.address.appartment);
+
+// Proprietățile unui obiect pot fi accesate utilizând notația cu punct sau notația cu paranteze drepte ([ ]):
+// brackets notation
+
+console.log(`person surname: ${person["surname"]}`);
+
+// Proprietățile unui obiect pot fi modificate sau adăugate prin atribuirea unei noi valori.
+// Modificare valori in obiect
+
+person.name = "Ion";
+console.log(`name: `, person.name);
+
+person.age = 31;
+person ["gender"] = "male";
+console.log(person);
+
+// Compararea Obiectelor - se compara ref obiectelor: ref a doua obiecte sunt diferite
+//Două obiecte sunt considerate egale doar dacă se referă la aceeași instanță a obiectului.
+// In variabile se tine o referinta catre obiect nu obiectul in sine
+
+const obj1 = {a: 10, b: true};
+const obj2 = {a: 10, b: true};
+const obj3 = obj1;
+// au aceeasi referinta
+console.log(`obj1 === obj2 => ${obj1 === obj2}`); // returneaza false deoarece avem doua obiecte cu aceeasi val dar instantele sunt diferite/ adrese diferite
+console.log(`obj1 === obj3 => ${obj1 === obj3}`); // returneaza true in obj3 salvam adresa catre obj1; doua copii ale aceleasi adrese
+
+// comparare dupa proprietati
+
+const areEqual = obj1.a === obj2.a && obj2.b === obj3.b;
+console.log(`areEqual: `, areEqual);
+
+
+
 
 
